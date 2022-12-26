0cc0698be889ae2aa0d36f465c84cde1203b465e
thdud <thgml1444@naver.com>
2022-12-26 10:51:11 +0900

    update

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -7,7 +7,6 @@ import Login from "../pages/Login";
 import PostPage from "../pages/PostPage";
 import NotFound from "../pages/NotFound";
 import UserEdit from "../pages/UserEdit";
-import Signup from "../pages/Signup";
 
 
 // import LoginPage from "../pages/LoginPage";
@@ -19,7 +18,6 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/mypage" element={<Mypage />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/postPage" element={<PostPage />} />
         <Route path="/userEdit" element={<UserEdit />} />
